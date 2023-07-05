66db9b3ab93f229512ac5971285d90f831bfa891
Leandro-Pinho <leandroessencia@gmail.com>
2023-07-05 11:09:51 -0300

    adicionando o footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import "./index.css"
 
 const Footer = () => {
   return (
     <div>
-      Footer
+
+      <div className='footer-superior'>
+        <h2>Brasil</h2>
+      </div>
+      <div className='footer-inferior'>
+        <div className='footer-list'>
+          <h2>Sobre</h2>
+          <h2>Publicidade</h2>
+          <h2>Negócios</h2>
+          <h2>Como funciona a Pesquisa</h2>
+        </div>
+        <div className='footer-list'>
+          <h2>Privacidade</h2>
+          <h2>Termos</h2>
+          <h2>Configurações</h2>
+        </div>
+      </div>
     </div>
   )
 }
